0c9201d9149478c74e6e46abda9ba1e35ae14861
Max <max@recoset.com>
2015-11-18 14:50:40 -0500

    dataset utility

---
+++ data.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class DataSet(object):
+	def __init__(self, filename, max_chars=-1):
+		text = [ord(char) for char in open(filename).read()[:max_chars]]
+		all_chars = list(set(data))
+		self.char_idx_map = {ordinal : i for i, ordinal in enumerate(all_chars)}
+		self.data = np.array([self.char_idx_map[o] for o in text])
+
+		valid_idx = int(len(all_chars) * 0.6)
+		test_idx = int(len(all_chars) * 0.8)
+
+		self.idx = {
+			"train" : (0, valid_idx),
+			"valid" : (valid_idx, test_idx),
+			"test" : (test_idx, -1)
+		}
+
+		self.n_chars = len(all_chars)
+
+
+	def yield_examples(self, dataset="train", batch_size=None):
+		if dataset not in ("train", "valid", "test"):
+			raise Exception("Invalid dataset")
+		
+		dataset_len = self.idx[dataset][1] - self.idx[dataset][0]
+
+		if batch_size is None:
+			batch_size = mode_len
+
+		n_batches = (dataset_len-1) / batch_size
+
+		for i in xrange(n_batches):
+			idx = self.idx[dataset][0] + i*batch_size
+			x = np.eye(n_chars)[self.data[idx:idx+batch_size]]
+			y = np.eye(n_chars)[self.data[idx+1:idx+batch_size+1]]
+			yield(x, y)
+			
