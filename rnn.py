7e3a6f61c0e95d7a4a1f3c59aec58d07f45102df
Max <max@recoset.com>
2015-11-30 17:21:51 -0500

    build_graph

--- rnn.py
+++ rnn.py
@@ -2,76 +2,85 @@ import tensorflow as tf
 import numpy as np
 
 class Cell(object):
-    def __init__(self, x_in, c_in, h_in, input_size, n_hidden, batch_size, cell_size):
+    def __init__(self, input_size, n_hidden, batch_size, cell_size):
         # HYPERPARAMETERS
         self.input_size = input_size
         self.n_hidden = n_hidden
         self.batch_size = batch_size
 
-        # tensors and placeholders
-        self.x_in = x_in
-        self.c_in = c_in
-        self.h_in = h_in
 
         # TODO: names ??
         # TODO: initialize biases as zeros ? (see Karpathy)
         # weights
-        self.w_f = w_f = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_f")
-        self.w_i = w_i = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_i")
-        self.w_c = w_c = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_c")
-        self.w_o = w_o = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_o")
+        self.w_f = w_f = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+1]), name="w_f")
+        self.w_i = w_i = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+1]), name="w_i")
+        self.w_c = w_c = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+1]), name="w_c")
+        self.w_o = w_o = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+1]), name="w_o")
 
         self.ones_for_bias_wgts = ones_for_bias_wgts = tf.constant(np.ones([batch_size,1,1]), name="b", dtype=tf.float32)
-
-        self.x_with_bias = x_with_bias = tf.concat(1, [ones_for_bias_wgts, x])
-        self.h_with_bias = h_with_bias = tf.concat(1, [ones_for_bias_wgts, h_in])
-        self.x_h_concat = x_h_concat = tf.concat(1, [h_with_bias, x_with_bias])
-
-        # forget gate layer
-        self.f = f = tf.sigmoid(tf.batch_matmul(w_f, x_h_concat))
-
-        # candidate values
-        self.i = i = tf.sigmoid(tf.batch_matmul(w_i, x_h_concat))
-        self.candidate_c = candidate_c = tf.tanh(tf.batch_matmul(w_c, x_h_concat))
-
-        # new cell state (hidden)
-        # forget old values of c
-        self.old_c_to_keep = old_c_to_keep = tf.mul(f, c_in)
-        # scaled candidate values of c
-        self.new_c_to_keep = new_c_to_keep = tf.mul(i, candidate_c)
-        self.c = c = tf.add(old_c_to_keep, new_c_to_keep)
-
-        # new scaled output
-        self.o = o = tf.sigmoid(tf.batch_matmul(w_o, x_h_concat))
-        self.h = h = tf.mul(o, tf.tanh(c))
-
 
         self.params = [w_f, w_i, w_c, w_o]
 
 
-def build_stack(stack_size, x_in, h_in, c_in, hyperparameters):
+    def build_node(self, scope="lstm_cell", x_in, c_in, h_in):
+        with tf.variable_scope(scope):
+            x_with_h = tf.concat(1, [x_in, h_in])
+            x_h_concat = tf.concat(1, [ones_for_bias_wgts, h_with_x])
+
+            # forget gate layer
+            f = tf.sigmoid(tf.batch_matmul(w_f, x_h_concat))
+
+            # candidate values
+            i = tf.sigmoid(tf.batch_matmul(w_i, x_h_concat))
+            candidate_c = tf.tanh(tf.batch_matmul(w_c, x_h_concat))
+
+            # new cell state (hidden)
+            # forget old values of c
+            old_c_to_keep = tf.mul(f, c_in)
+            # scaled candidate values of c
+            new_c_to_keep = tf.mul(i, candidate_c)
+            c = tf.add(old_c_to_keep, new_c_to_keep)
+
+            # new scaled output
+            o = tf.sigmoid(tf.batch_matmul(w_o, x_h_concat))
+            h = tf.mul(o, tf.tanh(c))
+            return (c, h)
+
+
+def build_graph():
     cells = []
-    cells.append(Cell(x_in, h_in, c_in, **hyperparameters))
-    for i` in xrange(stack_size-1):
+    for i in xrange(stack_size):
         with tf.variable_scope("Cell_{}".format(i)):
-            cells.append(Cell(cells[-1].h, , cells[-1].c, **hyperparameters))
+            cells.append(Cell(**hyperparameters[i]))
 
+    x_in = tf.placeholder(tf.float32, name="x", shape=(batch_size,input_size,n_steps))
+    y_in = tf.placeholder(tf.float32, name="y", shape=(batch_size,input_size,n_steps))
+    h_arr = [[tf.Variable(tf.zeros([batch_size,cell.n_hidden,1]), name="h_in") for cell in cells]]
+    c_arr = [[tf.Variable(tf.zeros([batch_size,cell.n_hidden,1]), name="c_in") for cell in cells]]
+    y_arr = []
+    
+    # list of lists of (x, c, h) tuples
+    # dimensions: n_steps by stack size by 3
+    for t in xrange(n_steps):
+        next_h = []
+        next_c = []
+        x = x_arr.slice(TODO)
 
+        for i, cell in enumerate(cells):
+            c, x = cell.build(scope="Cell_{}_t_{}".format(i,t), x_in=x, h_in=h_arr[i], c_in=c_arr[i])
+            next_c.append(c)
+            next_h.append(x)
 
-def build_graph(TODO):
-    x_in = tf.placeholder(tf.float32, name="x", shape=(batch_size,input_size,1))
-    # output from LSTM at t-1
-    h_in = tf.Variable(tf.random_normal([batch_size,n_hidden,1]), name="h_in")
-    # cell state from LSTM at t-1
-    c_in = tf.Variable(tf.zeros([batch_size,cell_size,1]), name="c_in")
+        h_arr = next_h
+        c_arr = next_c
+        y_arr.append(tf.nn.softmax(x))
 
     all_params = [param for cell in cells for param in cell.params]
+    cost = cross_entropy(tf.concat(2, y_arr), y_in)
+
     gradients = tf.GradientOptimizer(cost, all_params)
 
-    y_out = tf.nn.softmax(h)
 
-def add_cell_to(geriatric_cell, x, params):
-    return Cell(x, geriatric_cell.c, geriatric_cell.h, **params)
 
 def cross_entropy(observed, actual):
     return -tf.reduce_sum(actual*tf.log(observed))
@@ -86,19 +95,22 @@ def run_tf_sesh():
 
 
 def run_epoch(session):
+    
     # cross entropy
     cost_function = cross_entropy()
 
     with session as sesh:
 
 
-
-# hyperparameters
-input_size = 4
-n_hidden = 3
-batch_size = 1
-cell_size = 14
-
 if name == '__main__':
-    initialize_graph(input_size, n_hidden, batch_size, cell_size)
+
+
+    hyperparameters = {
+        "input_size": 4,
+        "n_hidden": 3,
+        "batch_size": 1,
+        "cell_size": 14
+    }
+    # initialize_graph(input_size, n_hidden, batch_size, cell_size)
+    cell = Cell()
     run_tf_sesh()
