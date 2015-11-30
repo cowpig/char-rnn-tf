97bf73dc5bfdbb5fb23cd265039352b143ed3a1a
Max <max@recoset.com>
2015-11-30 12:48:54 -0500

    working toward cell stacks

--- rnn.py
+++ rnn.py
@@ -1,57 +1,84 @@
 import tensorflow as tf
 import numpy as np
 
-def initialize_graph(n_inputs, n_hidden, batch_size, cell_size):
+class Cell(object):
+    def __init__(self, x_in, c_in, h_in, input_size, n_hidden, batch_size, cell_size):
+        # HYPERPARAMETERS
+        self.input_size = input_size
+        self.n_hidden = n_hidden
+        self.batch_size = batch_size
 
-    def build_layer(n_inputs, n_hidden, cell_size):
         # tensors and placeholders
-        x = tf.placeholder(tf.float32, name="x", shape=(batch_size,n_inputs,1))
+        self.x_in = x_in
+        self.c_in = c_in
+        self.h_in = h_in
 
         # TODO: names ??
         # TODO: initialize biases as zeros ? (see Karpathy)
         # weights
-        w_f = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+n_inputs+2]), name="w_f")
-        w_i = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+n_inputs+2]), name="w_i")
-        w_c = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+n_inputs+2]), name="w_c")
-        w_o = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+n_inputs+2]), name="w_o")
-        # output from LSTM at t-1
-        h_ = tf.Variable(tf.random_normal([batch_size,n_hidden,1]), name="h")
-        # cell state from LSTM at t-1
-        c_ = tf.Variable(tf.zeros([batch_size,cell_size,1]), name="c_")
+        self.w_f = w_f = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_f")
+        self.w_i = w_i = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_i")
+        self.w_c = w_c = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_c")
+        self.w_o = w_o = tf.Variable(tf.random_normal([batch_size,cell_size,n_hidden+input_size+2]), name="w_o")
 
-        ones_for_bias_wgts = tf.constant(np.ones([batch_size,1,1]), name="b", dtype=tf.float32)
+        self.ones_for_bias_wgts = ones_for_bias_wgts = tf.constant(np.ones([batch_size,1,1]), name="b", dtype=tf.float32)
 
-        x_with_bias = tf.concat(1, [ones_for_bias_wgts, x])
-        h_with_bias = tf.concat(1, [ones_for_bias_wgts, h_])
-        x_h_concat = tf.concat(1, [h_with_bias, x_with_bias])
+        self.x_with_bias = x_with_bias = tf.concat(1, [ones_for_bias_wgts, x])
+        self.h_with_bias = h_with_bias = tf.concat(1, [ones_for_bias_wgts, h_in])
+        self.x_h_concat = x_h_concat = tf.concat(1, [h_with_bias, x_with_bias])
 
         # forget gate layer
-        f = tf.sigmoid(tf.batch_matmul(w_f, x_h_concat))
+        self.f = f = tf.sigmoid(tf.batch_matmul(w_f, x_h_concat))
 
         # candidate values
-        i = tf.sigmoid(tf.batch_matmul(w_i, x_h_concat))
-        candidate_c = tf.tanh(tf.batch_matmul(w_c, x_h_concat))
+        self.i = i = tf.sigmoid(tf.batch_matmul(w_i, x_h_concat))
+        self.candidate_c = candidate_c = tf.tanh(tf.batch_matmul(w_c, x_h_concat))
 
         # new cell state (hidden)
         # forget old values of c
-        old_c_to_keep = tf.mul(f, c_)
+        self.old_c_to_keep = old_c_to_keep = tf.mul(f, c_in)
         # scaled candidate values of c
-        new_c_to_keep = tf.mul(i, candidate_c)
-        c = tf.add(old_c_to_keep, new_c_to_keep)
+        self.new_c_to_keep = new_c_to_keep = tf.mul(i, candidate_c)
+        self.c = c = tf.add(old_c_to_keep, new_c_to_keep)
 
         # new scaled output
-        o = tf.sigmoid(tf.batch_matmul(w_o, x_h_concat))
-        h = tf.mul(o, tf.tanh(c))
+        self.o = o = tf.sigmoid(tf.batch_matmul(w_o, x_h_concat))
+        self.h = h = tf.mul(o, tf.tanh(c))
 
-    # TODO: build multiple layers
 
-    # output
+        self.params = [w_f, w_i, w_c, w_o]
+
+
+def build_stack(stack_size, x_in, h_in, c_in, hyperparameters):
+    cells = []
+    cells.append(Cell(x_in, h_in, c_in, **hyperparameters))
+    for i` in xrange(stack_size-1):
+        with tf.variable_scope("Cell_{}".format(i)):
+            cells.append(Cell(cells[-1].h, , cells[-1].c, **hyperparameters))
+
+
+
+def build_graph(TODO):
+    x_in = tf.placeholder(tf.float32, name="x", shape=(batch_size,input_size,1))
+    # output from LSTM at t-1
+    h_in = tf.Variable(tf.random_normal([batch_size,n_hidden,1]), name="h_in")
+    # cell state from LSTM at t-1
+    c_in = tf.Variable(tf.zeros([batch_size,cell_size,1]), name="c_in")
+
+    all_params = [param for cell in cells for param in cell.params]
+    gradients = tf.GradientOptimizer(cost, all_params)
+
     y_out = tf.nn.softmax(h)
 
+def add_cell_to(geriatric_cell, x, params):
+    return Cell(x, geriatric_cell.c, geriatric_cell.h, **params)
+
+def cross_entropy(observed, actual):
+    return -tf.reduce_sum(actual*tf.log(observed))
 
 def run_tf_sesh():
     with tf.Session() as sesh:
-        data = np.ones((batch_size,n_inputs,1))
+        data = np.ones((batch_size,input_size,1))
         init = tf.initialize_all_variables()
         sesh.run(init)
         print sesh.run(c, feed_dict={x: data})
@@ -60,18 +87,18 @@ def run_tf_sesh():
 
 def run_epoch(session):
     # cross entropy
-    cost_function = -tf.reduce_sum(y_actual*tf.log(y_out))
+    cost_function = cross_entropy()
 
     with session as sesh:
 
 
 
 # hyperparameters
-n_inputs = 4
+input_size = 4
 n_hidden = 3
 batch_size = 1
 cell_size = 14
 
 if name == '__main__':
-    initialize_graph(n_inputs, n_hidden, batch_size, cell_size)
+    initialize_graph(input_size, n_hidden, batch_size, cell_size)
     run_tf_sesh()
