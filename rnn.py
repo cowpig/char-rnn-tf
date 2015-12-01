a66cc0361dad0eccbba6685080ac0dee9048bfd2
Max <max@recoset.com>
2015-12-01 18:45:22 -0500

    column vectors suck

--- rnn.py
+++ rnn.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 import numpy as np
 
 class Cell(object):
-    def __init__(self, input_size, n_hidden, batch_size, cell_size):
+    def __init__(self, input_size, output_size, batch_size):
         # HYPERPARAMETERS
         self.input_size = input_size
-        self.n_hidden = n_hidden
+        self.output_size = output_size
         self.batch_size = batch_size
 
         # weights
-        weight_dims = [batch_size, cell_size, n_hidden + input_size]
+        weight_dims = [batch_size, output_size, output_size + input_size]
         zeros_for_biases = tf.zeros(weight_dims[:-1] + [1])
 
         # print tf.concat(2, [tf.random_normal(weight_dims), zeros_for_biases])
@@ -25,19 +25,21 @@ class Cell(object):
 
 
     def build_node(self, x_in, c_in, h_in, scope="lstm_cell"):
-        print (x_in, c_in, h_in, scope)
-        print [type(thing) for thing in (x_in, c_in, h_in, scope)]
-        print [type(thing) for thing in h_in]
+        #print (x_in, c_in, h_in, scope)
+        #print [type(thing) for thing in (x_in, c_in, h_in, scope)]
+        # print [(item.name, item.dtype) for thing in (h_in, c_in) for item in thing]
+        # print (x_in.name, x_in.dtype)
+
         with tf.variable_scope(scope):
             x_with_h = tf.concat(1, [x_in, h_in])
-            x_h_concat = tf.concat(1, [ones_for_bias_wgts, h_with_x])
+            x_h_concat = tf.concat(1, [self.ones_for_bias_wgts, x_with_h])
 
             # forget gate layer
-            f = tf.sigmoid(tf.batch_matmul(w_f, x_h_concat))
+            f = tf.sigmoid(tf.batch_matmul(self.w_f, x_h_concat))
 
             # candidate values
-            i = tf.sigmoid(tf.batch_matmul(w_i, x_h_concat))
-            candidate_c = tf.tanh(tf.batch_matmul(w_c, x_h_concat))
+            i = tf.sigmoid(tf.batch_matmul(self.w_i, x_h_concat))
+            candidate_c = tf.tanh(tf.batch_matmul(self.w_c, x_h_concat))
 
             # new cell state (hidden)
             # forget old values of c
@@ -47,7 +49,7 @@ class Cell(object):
             c = tf.add(old_c_to_keep, new_c_to_keep)
 
             # new scaled output
-            o = tf.sigmoid(tf.batch_matmul(w_o, x_h_concat))
+            o = tf.sigmoid(tf.batch_matmul(self.w_o, x_h_concat))
             h = tf.mul(o, tf.tanh(c))
             return (c, h)
 
@@ -62,8 +64,8 @@ def build_graph(hyperparameters, n_steps, batch_size):
 
     x_in = tf.placeholder(tf.float32, name="x", shape=(batch_size,input_size,n_steps))
     y_in = tf.placeholder(tf.float32, name="y", shape=(batch_size,input_size,n_steps))
-    h_arr = [[tf.Variable(tf.zeros([batch_size,cell.n_hidden,1]), name="h_in") for cell in cells]]
-    c_arr = [[tf.Variable(tf.zeros([batch_size,cell.n_hidden,1]), name="c_in") for cell in cells]]
+    h_arr = [tf.Variable(tf.zeros([batch_size,cell.output_size,1]), name="h_in") for cell in cells]
+    c_arr = [tf.Variable(tf.zeros([batch_size,cell.input_size,1]), name="c_in") for cell in cells]
     y_arr = []
     
     # list of lists of (x, c, h) tuples
@@ -80,12 +82,13 @@ def build_graph(hyperparameters, n_steps, batch_size):
 
         h_arr = next_h
         c_arr = next_c
-        y_arr.append(tf.nn.softmax(x))
+        vec = tf.nn.softmax(tf.squeeze(x, [2]))
+        y_arr.append(tf.expand_dims(vec, 2))
 
     all_params = [param for cell in cells for param in cell.params]
     cost = cross_entropy(tf.concat(2, y_arr), y_in)
 
-    gradients = tf.GradientOptimizer(cost, all_params)
+    gradients = tf.train.GradientDescentOptimizer(cost, all_params)
 
     return (x_in, y_in, all_params, cost, gradients)
 
@@ -109,15 +112,14 @@ def run_tf_sesh():
 
 if __name__ == '__main__':
 
-    n_steps = 4
+    n_steps = 2
     batch_size = 1
     input_size = 3
     stack_size = 2
     hyperparameters = [{
         "input_size": input_size,
-        "n_hidden": input_size,
+        "output_size": input_size,
         "batch_size": batch_size,
-        "cell_size": 14
     } for _ in xrange(stack_size)]
 
     x = np.array([[[0,1,0],[1,0,0]]])
