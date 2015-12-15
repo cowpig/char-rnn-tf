8a7478e348a90b52cf7ab4163fb24a16a7f1254e
Miriam Shiffman <meereeum@gmail.com>
2015-12-15 18:57:24 -0500

    train on you crazy diamond

--- metamodel.py
+++ metamodel.py
@@ -57,77 +57,66 @@ if __name__ == "__main__":
     graph = rnn.build_graph(config)
 
     with tf.Session() as sesh:
-        # logger = tf.train.SummaryWriter('./log', sesh.graph_def)
-        # pylogger = tf.python.training.summary_io.SummaryWriter('./pylog', sesh.graph_def)
 
         sesh.run(tf.initialize_all_variables())
         t = graph['train']
         test = graph['test']
         train_state = np.zeros([batch_size, rnn.get_state_size(config)])
 
-        # tf.train.write_graph(sesh.graph_def, './graph', 'rnn_graph.pbtxt')
-
-        # training = {
-        #     'x_in': x_in,
-        #     'y_in': y_in,
-        #     'states_in': states_in,
-        #     'states_out': states_out,
-        #     'cost' : cost,
-        #     'train_op': train_op
-        # }
-
         cost = np.inf
         itr=0
 
         testing_generator = dataset.yield_examples(dataset="test")
 
-        try:
-          for (x,y) in dataset.yield_examples(steps=n_steps):
-              train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
-                                              feed_dict={t['x_in']:np.array([x]),
-                                                        t['y_in']:np.array([y]),
-                                                        t['states_in']:train_state})
+        while True:
+          try:
+              for (x,y) in dataset.yield_examples(steps=n_steps):
+                  train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
+                                                  feed_dict={t['x_in']:np.array([x]),
+                                                            t['y_in']:np.array([y]),
+                                                            t['states_in']:train_state})
 
+                  if itr % 100 == 0:
+                      print "cost at epoch {}: {}".format(itr, cost)
 
-              if itr % 100 == 0:
-                  print "cost at epoch {}: {}".format(itr, cost)
+                      if itr % 1000 == 0:
+                          test_cost = 0
+                          test_state = np.zeros([1, rnn.get_state_size(config)])
+                          print "testing..."
+                          inputs = []
+                          outputs = []
+                          for _ in xrange(30):
+                              x, y = testing_generator.next()
+                              inputs.append(x[:])
+                              test_state, x, cost = sesh.run([test['states_out'],
+                                                              test['y_out'],
+                                                              test['cost']],
+                                                            feed_dict={
+                                                                test['x_in']:x,
+                                                                test['states_in']:test_state,
+                                                                test['y_in']:y})
+                              test_cost += cost
+                              outputs.append(x[0][:])
 
-              if itr % 1000 == 0:
-                  test_cost = 0
-                  test_state = np.zeros([1, rnn.get_state_size(config)])
-                  print "testing..."
-                  inputs = []
-                  outputs = []
-                  for _ in xrange(30):
-                      x, y = testing_generator.next()
-                      inputs.append(x[:])
-                      test_state, x, cost = sesh.run([test['states_out'], test['y_out'],
-                                                      test['cost']],
-                                                feed_dict={test['x_in']:x,
-                                                          test['states_in']:test_state,
-                                                          test['y_in']:y})
-                      test_cost += cost
-                      outputs.append(x[0][:])
+                              print "Test cost: ", test_cost
 
-                  print "Test cost: ", test_cost
+                              readable_x = u''.join(dataset.convert(
+                                  dataset.data_to_ords(inputs)))
+                              print "input: ", readable_x
 
-                  readable_x = u''.join(dataset.convert(dataset.data_to_ords(inputs)))
+                              indexed_probs = [sorted([idxed for idxed in enumerate(letter)],
+                                                      key=lambda x: x[1], reverse=True)
+                                              for letter in outputs]
+                              top_5_idxs = [i[:5] for i in indexed_probs]
 
-                  print "input: ", readable_x
-
-                  indexed_probs = [sorted([idxed for idxed in enumerate(letter)],
-                                              key=lambda x: x[1], reverse=True)
-                                    for letter in outputs]
-                  top_5_idxs = [i[:5] for i in indexed_probs]
-
-                  for char, top5 in zip(readable_x, top_5_idxs):
-                      print "'",char,"'"
-                      top_out = [u"{0}: {1:.3f}".format(dataset.convert(l[0]), l[1])
-                                  for l in top5]
-                      print u"\t", u" | ".join(top_out).replace("\n", "\\n")
+                              for char, top5 in zip(readable_x, top_5_idxs):
+                                  print "'",char,"'"
+                                  top_out = [u"{0}: {1:.3f}".format(dataset.convert(l[0]), l[1])
+                                            for l in top5]
+                                  print u"\t", u" | ".join(top_out).replace("\n", "\\n")
 
                   itr+=1
 
-        except KeyboardInterrupt:
-            print dataset.plaintext_dataset()
-            import sys;sys.exit(0)
+          except KeyboardInterrupt:
+              print dataset.plaintext_dataset()
+              import sys;sys.exit(0)
