38e659a250972176cff2b6a4d8197ebd315524aa
Max <max@recoset.com>
2015-12-16 16:26:52 -0500

    temp commit

--- metamodel.py
+++ metamodel.py
@@ -6,7 +6,55 @@ from layers import LSTM, FullyConnected
 import sys
 import os.path
 
+def score(sesh, config, graph, dataset, mode="valid", n_examples=50):
+    costs = []
+    outs = []
+    state = np.zeros([1, rnn.get_state_size(config)])
 
+    for i, (x, y) in enumerate(dataset.yield_examples(dataset=mode)):
+        state, out, cost = sesh.run([graph['states_out'], graph['y_out'], graph['cost']],
+                                    feed_dict={ graph['x_in']:x, 
+                                                graph['states_in']:state,
+                                                graph['y_in']:y })
+        costs.append(cost)
+
+
+
+def generate_text(sesh, config, graph, dataset, mode="valid", n_examples=50):
+    test_cost = 0
+    test_state = np.zeros([1, rnn.get_state_size(config)])
+    print "testing..."
+    inputs = []
+    outputs = []
+    for _ in xrange(30):
+        x, y = testing_generator.next()
+        inputs.append(x[:])
+        test_state, x, cost = sesh.run([test['states_out'],
+                                          test['y_out'],
+                                          test['cost']],
+                                        feed_dict={
+                                            test['x_in']:x,
+                                            test['states_in']:test_state,
+                                            test['y_in']:y})
+        test_cost += cost
+        outputs.append(x[0][:])
+
+        print "Test cost: ", test_cost
+
+        readable_x = u''.join(dataset.convert(
+              dataset.data_to_ords(inputs)))
+        print "input: ", readable_x
+
+        indexed_probs = [sorted([idxed for idxed in enumerate(letter)],
+                                  key=lambda x: x[1], reverse=True)
+                          for letter in outputs]
+        top_5_idxs = [i[:5] for i in indexed_probs]
+
+        for char, top5 in zip(readable_x, top_5_idxs):
+            print "'",char,"'"
+            top_out = [u"{0}: {1:.3f}".format(dataset.convert(l[0]), l[1])
+                        for l in top5]
+            print u"\t", u" | ".join(top_out).replace("\n", "\\n")
 
 
 if __name__ == "__main__":
@@ -86,53 +134,23 @@ if __name__ == "__main__":
 
 
         while True:
-          try:
-              for (x,y) in dataset.yield_examples(steps=n_steps):
-                  train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
+            try:
+                for t, (x,y) in enumerate(dataset.yield_examples(steps=n_steps)):
+                    train_costs = 
+                    train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
                                                   feed_dict={t['x_in']:np.array([x]),
                                                             t['y_in']:np.array([y]),
                                                             t['states_in']:train_state})
+                    if t % 100 == 0:
+                        print "\ttraining cost at epoch {} step {}:\t{}".format(itr, t, cost)
 
-                  if itr % 100 == 0:
-                      print "cost at epoch {}: {}".format(itr, cost)
+                if itr % 10 == 0:
 
-                      if itr % 1000 == 0:
-                          test_cost = 0
-                          test_state = np.zeros([1, rnn.get_state_size(config)])
-                          print "testing..."
-                          inputs = []
-                          outputs = []
-                          for _ in xrange(30):
-                              x, y = testing_generator.next()
-                              inputs.append(x[:])
-                              test_state, x, cost = sesh.run([test['states_out'],
-                                                              test['y_out'],
-                                                              test['cost']],
-                                                            feed_dict={
-                                                                test['x_in']:x,
-                                                                test['states_in']:test_state,
-                                                                test['y_in']:y})
-                              test_cost += cost
-                              outputs.append(x[0][:])
 
-                              print "Test cost: ", test_cost
+                if itr % 50 == 0:
 
-                              readable_x = u''.join(dataset.convert(
-                                  dataset.data_to_ords(inputs)))
-                              print "input: ", readable_x
 
-                              indexed_probs = [sorted([idxed for idxed in enumerate(letter)],
-                                                      key=lambda x: x[1], reverse=True)
-                                              for letter in outputs]
-                              top_5_idxs = [i[:5] for i in indexed_probs]
-
-                              for char, top5 in zip(readable_x, top_5_idxs):
-                                  print "'",char,"'"
-                                  top_out = [u"{0}: {1:.3f}".format(dataset.convert(l[0]), l[1])
-                                            for l in top5]
-                                  print u"\t", u" | ".join(top_out).replace("\n", "\\n")
-
-                  itr+=1
+                      itr+=1
 
           except KeyboardInterrupt:
             import datetime
