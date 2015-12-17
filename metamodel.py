f336dda6b32149be349cc559636570a7b1813ec3
Miriam Shiffman <meereeum@gmail.com>
2015-12-16 21:56:30 -0500

    testing rnn.riff

--- metamodel.py
+++ metamodel.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     dataset = data.DataSet('./data/edgar.txt',decoding_fx=data.decode_encoding)
     # dataset.idx['train'] = (0,320)
 
-    input_size = dataset.n_chars
+    input_size = dataset.n_chars+2
     n_steps = 16
     batch_size = 1
     n_hidden = 100
@@ -64,54 +64,56 @@ if __name__ == "__main__":
         }
     }
 
-    graph = rnn.build_graph(config)
+    rnn.riff(fn, config, dataset)
 
-    with tf.Session() as sesh:
-        sesh.run(tf.initialize_all_variables())
-
-        saver = tf.train.Saver()
-        if fn:
-            saver.restore(sesh, fn)
-
-        t = graph['train']
-        test = graph['test']
-        train_state = np.zeros([batch_size, rnn.get_state_size(config)])
-
-        cost = np.inf
-        itr=0
-
-        testing_generator = dataset.yield_examples(dataset="test")
-
-
-        while True:
-            try:
-                train_costs = []
-                for step, (x,y) in enumerate(dataset.yield_examples(steps=n_steps)):
-                    train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
-                                                  feed_dict={t['x_in']:np.array([x]),
-                                                            t['y_in']:np.array([y]),
-                                                            t['states_in']:train_state})
-                    train_costs.append(cost)
-                    # if step % 200 == 0:
-                    #     print "\ttraining cost at epoch {} step {}:\t{}".format(itr, step, cost)
-
-                print "Avg cost for training epoch {}: {}".format(itr, np.mean(train_costs))
-
-                if itr % 20 == 0:
-                    rnn.print_score(sesh, config, test, dataset, mode="valid")
-                elif itr % 5 == 0:
-                    rnn.print_score(sesh, config, test, dataset, mode="valid", n_to_print=0)
-
-
-                itr+=1
-
-            except KeyboardInterrupt:
-                import datetime
-                q = raw_input("Type 'Y' or 'y' to see save model & see test score:\n")
-                if q.lower() == 'y':
-                    fn = "models/metamodel-{}.ckpt".format(datetime.datetime.now().strftime("%m-%d_%Hh%M"))
-                    saver.save(sesh, fn)
-                    print "model saved at: {}".format(fn)
-
-                    rnn.print_score(sesh, config, test, dataset, mode="test")
-                sys.exit(0)
+    #graph = rnn.build_graph(config)
+#
+    #with tf.Session() as sesh:
+        #sesh.run(tf.initialize_all_variables())
+#
+        #saver = tf.train.Saver()
+        #if fn:
+            #saver.restore(sesh, fn)
+#
+        #t = graph['train']
+        #test = graph['test']
+        #train_state = np.zeros([batch_size, rnn.get_state_size(config)])
+#
+        #cost = np.inf
+        #itr=0
+#
+        #testing_generator = dataset.yield_examples(dataset="test")
+#
+#
+        #while True:
+            #try:
+                #train_costs = []
+                #for step, (x,y) in enumerate(dataset.yield_examples(steps=n_steps)):
+                    #train_state, cost, _ = sesh.run([t['states_out'], t['cost'], t['train_op']],
+                                                  #feed_dict={t['x_in']:np.array([x]),
+                                                            #t['y_in']:np.array([y]),
+                                                            #t['states_in']:train_state})
+                    #train_costs.append(cost)
+                    ## if step % 200 == 0:
+                    ##     print "\ttraining cost at epoch {} step {}:\t{}".format(itr, step, cost)
+#
+                #print "Avg cost for training epoch {}: {}".format(itr, np.mean(train_costs))
+#
+                #if itr % 20 == 0:
+                    #rnn.print_score(sesh, config, test, dataset, mode="valid")
+                #elif itr % 5 == 0:
+                    #rnn.print_score(sesh, config, test, dataset, mode="valid", n_to_print=0)
+#
+#
+                #itr+=1
+#
+            #except KeyboardInterrupt:
+                #import datetime
+                #q = raw_input("Type 'Y' or 'y' to see save model & see test score:\n")
+                #if q.lower() == 'y':
+                    #fn = "models/metamodel-{}.ckpt".format(datetime.datetime.now().strftime("%m-%d_%Hh%M"))
+                    #saver.save(sesh, fn)
+                    #print "model saved at: {}".format(fn)
+#
+                    #rnn.print_score(sesh, config, test, dataset, mode="test")
+                #sys.exit(0)
